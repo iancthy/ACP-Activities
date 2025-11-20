af3261e42a522c676a0b04717391aee70081db02
Gian Carlo Sanchez <164422423+iancthy@users.noreply.github.com>
2025-11-20 09:07:34 +0800

    Create activity.py

---
+++ Decision Making/activity.py
@@ -0,0 +1,10 @@
+num = int(input("Enter your grade (0-100): "))
+
+if num >= 90:
+    print("Grade: A")
+elif num >= 80:
+    print("Grade: B")
+elif num >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")
