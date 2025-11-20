70b953422b4b898dbe7c6cdc0bd516c3c3d519a7
Gian Carlo Sanchez <164422423+iancthy@users.noreply.github.com>
2025-11-20 08:57:51 +0800

    Create activity.py

---
+++ Basic Mathematical Operation/activity.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 5
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+prod_result = num1 * num2
+if num2 != 0:
+    div_result = num1 / num2
+else:
+    div_result = "Error! Division by zero."
+
+print("Addition: " + str(sum_result))
+print("Subtraction: " + str(diff_result))
+print("Multiplication: " + str(prod_result))
+print("Division: " + str(div_result))
